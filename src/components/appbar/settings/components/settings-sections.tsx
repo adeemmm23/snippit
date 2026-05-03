925a935061a93127515c7dba4e5c9f4842b5c55c
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-05-03 21:49:32 +0100

    feat: add some settings UI and added scroll to the Main Setting Section

--- src/components/appbar/settings/components/settings-sections.tsx
+++ src/components/appbar/settings/components/settings-sections.tsx
@@ -1,15 +1,24 @@
+import MagicSettingGroup from "./groups/magic";
 import ThemeSettingGroup from "./groups/theme";
+import VariableSettingGroup from "./groups/variable";
 import SettingSection from "./ui/setting-section";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function SettingsSections() {
   return (
-    <main className="flex flex-1 flex-col gap-10">
-      <SettingSection title="Appearance">
-        <ThemeSettingGroup />
-      </SettingSection>
-      <SettingSection title="Editor">
-        <div></div>
-      </SettingSection>
+    <main className="h-full flex-1">
+      <ScrollArea className="size-full overflow-auto">
+        <div className="flex flex-col gap-10">
+          <SettingSection title="Appearance">
+            <ThemeSettingGroup />
+          </SettingSection>
+          <SettingSection title="Editor">
+            <VariableSettingGroup />
+            <MagicSettingGroup />
+          </SettingSection>
+        </div>
+      </ScrollArea>
     </main>
   );
 }
--- src/components/appbar/settings/settings.tsx
+++ src/components/appbar/settings/settings.tsx
@@ -15,22 +15,22 @@ import { Separator } from "@/components/ui/separator";
 export default function Settings() {
   return (
     <DialogContent
-      className="bg-popover flex h-11/12 w-full max-w-5xl! flex-col"
+      className="bg-popover flex h-full max-h-11/12 w-full max-w-5xl! flex-col"
       showCloseButton={false}
     >
-      <DialogHeader className="shrink">
+      <DialogHeader className="shrink-0">
         <DialogTitle className="text-2xl">Settings</DialogTitle>
         <DialogDescription>
           Here you can modify themes, editor behavior, and other options to make
           Snippit truly yours.
         </DialogDescription>
       </DialogHeader>
-      <div className="flex h-full flex-1 grow gap-2 py-2">
+      <div className="flex min-h-0 flex-1 grow gap-2 py-2">
         <SettingsNavigation />
         <Separator orientation="vertical" className="mx-4" />
         <SettingsSections />
       </div>
-      <DialogFooter>
+      <DialogFooter className="shrink-0">
         <DialogClose render={<Button variant="outline">Cancel</Button>} />
         <Button type="submit">Save changes</Button>
       </DialogFooter>
