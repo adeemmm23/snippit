0186b787e1663e4a192466edfca6994bf5cfdcb0
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-05-09 00:16:28 +0100

    feat: create a draft for Editor variable

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,13 @@
-export const VARIABLE_REGEX = /\{([a-zA-Z0-9_]+)\}/g;
+export const VARIABLE_FORMATS: { label: string; value: RegExp }[] = [
+  { label: "{variable}", value: /\{([a-zA-Z0-9_ ]+)\}/g },
+  { label: "{{variable}}", value: /\{\{([a-zA-Z0-9_ ]+)\}\}/g },
+  { label: "%variable%", value: /%([a-zA-Z0-9_ ]+)%/g },
+  { label: "%%variable%%", value: /%%([a-zA-Z0-9_ ]+)%%/g },
+  { label: "<variable>", value: /<([a-zA-Z0-9_ ]+)>/g },
+  { label: "((variable))", value: /\(\(([a-zA-Z0-9_ ]+)\)\)/g },
+  { label: "[variable]", value: /\[([a-zA-Z0-9_ ]+)\]/g },
+  { label: "[[variable]]", value: /\[\[([a-zA-Z0-9_ ]+)\]\]/g },
+  { label: "${variable}", value: /\$\{([a-zA-Z0-9_ ]+)\}/g },
+];
 
 export const LOCAL_STORAGE_KEY = "files";
