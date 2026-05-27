a14091cd0d3b2bc0f4203942ff929d9e6bf3a2d2
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-05-27 18:46:26 +0100

    fix: add getAvailableName util

--- src/stores/files/utils.ts
+++ src/stores/files/utils.ts
@@ -1,1 +1,20 @@
-// import { isFolder, type NodeType } from "@/types/node.types";
+import { type NodeType } from "@/types/node.types";
+
+export const getAvailableName = (
+  items: NodeType[],
+  baseName: string,
+): string => {
+  if (!items.find((item) => item.name === baseName)) {
+    return baseName;
+  }
+
+  let counter = 1;
+  let finalName = baseName;
+
+  while (items.find((item) => item.name === finalName)) {
+    finalName = `${baseName} ${counter}`;
+    counter++;
+  }
+
+  return finalName;
+};
