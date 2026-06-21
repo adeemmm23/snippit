a03c1d2e077f76a541e95db0fe3fd45acda789a7
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-06-21 21:46:23 +0100

    fix: replaced the bottom padding with div to allow last secion to be pushed all the way to the top

--- src/features/settings/components/ui/setting-section.tsx
+++ src/features/settings/components/ui/setting-section.tsx
@@ -1,17 +1,21 @@
+import { cn } from "@/utils/cn";
+
 type SettingSectionProps = {
   title: string;
   children: React.ReactNode;
+  last?: boolean;
 };
 
 export default function SettingSection({
   title,
   children,
+  last,
 }: SettingSectionProps) {
   return (
     <section
       id={title}
       data-section={title}
-      className="flex scroll-mt-3 flex-col gap-2"
+      className={cn("flex scroll-mt-3 flex-col gap-2", last && "h-72")}
     >
       <h2 className="mb-2 text-lg font-semibold">{title}</h2>
       <div className="flex flex-col gap-10">{children}</div>
